87b5b3ff28f572a1827eb553e124aee3be0a39ef
Joost van Dijk <argine@xs4all.nl>
2013-10-23 17:14:03 +0200

    make setup protocol-independent

--- config.php
+++ config.php
@@ -3,8 +3,8 @@ $host = isset($_SERVER['HTTP_HOST']) ? $_SERVER['HTTP_HOST'] : 'localhost:8080';
 
 $proto = 'http';
 
-if (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on')
-{
+if (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off'
+    || $_SERVER['SERVER_PORT'] == 443) {
     $proto = 'https';
 }
 
