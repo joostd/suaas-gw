81fed5a7aabcf8485018f4ac00c22a34cabd8de6
Joost van Dijk <joost.vandijk@surfnet.nl>
2013-10-24 12:26:40 +0200

    add index page

---
+++ www/index.php
@@ -0,0 +1,7 @@
+<h1>Step-up  gateway</h1>
+
+This is the prototype proxy for SURFnet's step-up authentication service.
+<p>
+You can test using the local <a href="sp">demo SP</a>.
+To be able to authenticate using a second factor, you will first need to <a href="https://suaas-ra.surfnet.nl/">register</a> your token.
+</p>
