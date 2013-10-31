e3d34a6701f2cb53d65279ad5f443a4e9cd47c26
Joost van Dijk <argine@xs4all.nl>
2013-10-31 10:26:15 +0100

    add signing cert to idp metadata

--- www/metadata.php
+++ www/metadata.php
@@ -1,12 +1,27 @@
 <?php
 include('../config.php');
+include_once(dirname(dirname(__FILE__)) . '/vendor/xmlseclibs/xmlseclibs.php');
+
+$entityID = $config['entity_id'];
+$certfile = $config['certfile'];
+
+$cert = '';
+if( file_exists($certfile)) {
+    $cert = file_get_contents($certfile);
+}
+$cert = XMLSecurityDSig::staticGet509XCerts($cert);
+$keydescriptor = '';
+if( isset($cert[0]) ) $keydescriptor = <<<XML
+    <KeyDescriptor xmlns:ds="http://www.w3.org/2000/09/xmldsig#" use="signing">
+      <ds:KeyInfo>
+        <ds:X509Data>
+          <ds:X509Certificate>$cert[0]</ds:X509Certificate>
+        </ds:X509Data>
+      </ds:KeyInfo>
+    </KeyDescriptor>
+XML;
 
 $uri = isset($_SERVER['REQUEST_URI']) ? $_SERVER['REQUEST_URI'] : '/metadata';
-
-// convention: use metadata URL as entity ID
-//$entityID = "$proto://$host$uri";
-$entityID = $config['entity_id'];
-
 $base = dirname($uri);
 $acs_location = "$proto://$host$base/acs.php";
 $sso_location = "$proto://$host$base/sso.php";
@@ -14,6 +29,7 @@ $sso_location = "$proto://$host$base/sso.php";
 $xml = <<<XML
 <EntityDescriptor xmlns="urn:oasis:names:tc:SAML:2.0:metadata" entityID="$entityID">
   <IDPSSODescriptor protocolSupportEnumeration="urn:oasis:names:tc:SAML:2.0:protocol">
+    $keydescriptor
     <SingleSignOnService Binding="urn:oasis:names:tc:SAML:2.0:bindings:HTTP-Redirect" Location="$sso_location"/>
   </IDPSSODescriptor>
   <SPSSODescriptor protocolSupportEnumeration="urn:oasis:names:tc:SAML:2.0:protocol">
